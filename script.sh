1b19575294a16d4e280c076261093b8ea4a5d209
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-05-02 17:55:59 -0500

    Added folder

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
-folders=(
-"Algorithms/Hashing-Encryption/HashFunction" "Algorithms/Hashing-Encryption/RSA-AES"  )
+folders=("Algorithms/String/KMP" "Algorithms/String/RabinKarp" "Algorithms/String/trie" "Algorithms/Scheduling/RoundRobin" "Algorithms/Scheduling/ExponentialBackoff" "Algorithms/Most-know/BigONotation" "Algorithms/Most-know/Memoization" "Algorithms/Most-know/DivideAndConquer" "Algorithms/Most-know/SlidingWindow" "Algorithms/Most-know/TwoPointers" "Algorithms/Most-know/Union-find")
 for folder in "${folders[@]}"; do
   mkdir -p "$folder"
   touch "$folder"/.gitkeep
