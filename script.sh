e06960ee943c5aa7784e9b5a058103ebb06e7fd2
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-05-02 17:35:54 -0500

    Added folder

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
-folders=("test/one" "test/two")
+folders=("test/three" "test/four")
 
 for folder in "${folders[@]}"; do
   mkdir -p "$folder"
   touch "$folder"/.gitkeep
+
+git add .
+git commit -m "Added folder"
+git push origin main 
 done
