080ac27ddef509d9aa6dfe110837d2a56275d277
Andres Rubiano Del Chiaro <andres.rubiano@globant.com>
2025-05-02 17:58:02 -0500

    Deleted no longer file

--- script.sh
+++
@@ -1,9 +0,0 @@
-folders=("Algorithms/String/KMP" "Algorithms/String/RabinKarp" "Algorithms/String/trie" "Algorithms/Scheduling/RoundRobin" "Algorithms/Scheduling/ExponentialBackoff" "Algorithms/Most-know/BigONotation" "Algorithms/Most-know/Memoization" "Algorithms/Most-know/DivideAndConquer" "Algorithms/Most-know/SlidingWindow" "Algorithms/Most-know/TwoPointers" "Algorithms/Most-know/Union-find")
-for folder in "${folders[@]}"; do
-  mkdir -p "$folder"
-  touch "$folder"/.gitkeep
-
-git add .
-git commit -m "Added folder"
-git push origin main 
-done
